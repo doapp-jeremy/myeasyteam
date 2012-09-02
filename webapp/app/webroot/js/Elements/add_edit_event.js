27b6e22d1290d84b62c1b1e9de1cc95b6d9d5226
Jeremy McJunkin <jeremy@doapps.com>
2012-09-02 13:48:09 -0500

    set end time +1 hour from start time

--- webapp/app/webroot/js/Elements/add_edit_event.js
+++ webapp/app/webroot/js/Elements/add_edit_event.js
@@ -1,7 +1,24 @@
 $(function() {
   
+  var format = "%Y-%m-%d %h:%i%p";
   //$('input[name="date"]').daterangepicker();  
   
-  AnyTime.picker( "start", { format: "%Y-%m-%d %h:%i%p", firstDOW: 1, 'earliest':  new Date()} );
-  AnyTime.picker( "end", { format: "%Y-%m-%d %h:%i%p", firstDOW: 1, 'earliest': new Date() } );
+  AnyTime.picker( "start", { format: format, firstDOW: 1, 'earliest':  new Date()} );
+  AnyTime.picker( "end", { format: format, firstDOW: 1, 'earliest': new Date() } );
+  
+  // attach to change event for start to update end
+  // TODO: attach to close of dialog
+  $('#start').bind('change', function(){
+    var defaultConv = new AnyTime.Converter({ format: format });
+    var start = defaultConv.parse($('#start').val());
+    var end = $('#end').val();
+    //if (!end)
+    {
+      var end = start;
+      end.setHours(start.getHours() + 1);
+      $('#end').val(defaultConv.format(end));
+    }
+    
+    
+  });
 });
